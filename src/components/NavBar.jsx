3eda7b0bacc67ec1efdf0cbf0153b9c9452f0716
David Gurovich <david.gurovich@outlook.com>
2023-01-23 14:14:05 -0800

    created mobile menu using state and map

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {
+  VscClose,
   VscThreeBars,
   VscHome,
   VscPerson,
@@ -8,10 +9,10 @@ import {
 } from 'react-icons/vsc';
 
 const navLinks = [
-  {name: "Home", link: "#home", logo: {VscHome}},
-  {name: "About", link: "#about", logo: {VscPerson}},
-  {name: "Home", link: "#projects", logo: {VscProject}},
-  {name: "Home", link: "#contact", logo: {VscMail}},
+  {name: "Home", link: "#home", logo: <VscHome/>},
+  {name: "About", link: "#about", logo: <VscPerson/>},
+  {name: "Projects", link: "#projects", logo: <VscProject/>},
+  {name: "Contact", link: "#contact", logo: <VscMail/>},
 
 ]
 function NavBar() {
@@ -19,17 +20,36 @@ function NavBar() {
   const handleClick = () => setNav(!nav)
 
   return (
-    <header className='text-gray-300 fixed h-[70px] w-full flex justify-between items-center px-4'>
-      
-        
-            <ul className='md:flex hidden'>
-                  <a href="#home" className='hover:text-red-600'>Home</a>
-                  <a href= "#about" className='hover:text-red-600'>About</a>
-                  <a href="#projects" className='hover:text-red-600'>Projects</a>
-                  <a href="#contact" className='hover:text-red-600'>Contact</a>
+    <header className='text-gray-300 fixed h-[70px] w-full flex justify-end items-center px-4'>
+            {/* Mobile three bars logo for menu */}
+
+            <div className='md:hidden z-10' onClick={handleClick}>
+                {!nav ? <VscThreeBars/> : <VscClose/>}
+            </div>
+
+            <ul className='md:flex md:flex-end hidden'>
+              {navLinks.map((nav, i) => (
+                <li key={i} className='hover:text-red-600 flex gap-5'>
+                  <div>{nav.logo}</div>
+                  <a href={nav.link}> {nav.name}</a>
+                </li>
+              ))}
             </ul>
 
-       
+            {/* menu for a mobile device/ smaller screen*/}
+            <ul className={
+              !nav ? 'hidden': 
+              'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            }
+            >
+              {navLinks.map((nav, i) => (
+                <li key={i} className='hover:text-red-600 flex gap-5 py-6'>
+                  <div>{nav.logo}</div>
+                  <a href={nav.link} onClick={handleClick}> {nav.name}</a>
+                </li>
+              ))}
+            </ul>
+
       
     </header>
   )
