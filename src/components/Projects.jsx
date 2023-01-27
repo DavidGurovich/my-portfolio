d87aa093a10d438b639c633dc29a1f30b2ad1ecd
David Gurovich <david.gurovich@outlook.com>
2023-01-26 16:01:03 -0800

    time to fix mobile view (projects page)

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,21 +12,22 @@ function Projects() {
           <div className='flex flex-row justify-center gap-5 px-20' >
               <img src={weatherPhoto} alt="" className=" rounded-3xl shadow-lg shadow-blue-500/60"/>
               <div className=' pt-10'>
+              <h1 className='text-white text-3xl font-bold'> Weather App</h1>
                 <p className='text-2xl text-white'> A weather app created using ReactJS, TailWindCSS, and the OpenWeatherMap API.  </p> 
-                <div className='flex flex-row gap-5'>
-                  <a href="" className=''>Live Project</a>
-                  <a href="" className=''>View Code</a>
+                <div className='flex flex-col gap-5 pt-10 font-bold'>
+                  <a href="" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-neutral-900 text-white'>Live Project</a>
+                  <a href="" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-black text-white'>View Code</a>
                 </div>
               </div>
           </div>
           <div className='flex flex-row justify-center gap-5 px-20' >
               <div className=' pt-10'>
-                <h1 className=''> JavaScript Calculator</h1>
-                <p className='text-2xl text-white '>One of the first projects I created while learning
+                <h1 className='text-white text-3xl font-bold'> JavaScript Calculator</h1>
+                <p className='text-2xl text-white relative'>One of the first projects I created while learning
                 programming through the Odin Project. Made using only JavaScript, CSS, and HTML. I avoided using the </p>
-                <div className='flex flex-row gap-5 pt-10'>
-                  <a href="" className='px-5 py-2 border-2 rounded-lg hover:border-red-600 text-white'>Live Project</a>
-                  <a href="" className='px-5 py-2 border-2 rounded-lg hover:border-red-600 text-white'>View Code</a>
+                <div className='flex flex-col gap-5 pt-10 font-bold'>
+                  <a href="" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-neutral-900 text-white'>Live Project</a>
+                  <a href="" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-black text-white'>View Code</a>
                 </div>
               </div>
               <img src={calcPhoto} alt="" className=" rounded-3xl shadow-lg shadow-red-500/60"/>
