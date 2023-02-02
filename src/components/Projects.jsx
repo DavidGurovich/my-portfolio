770defb1762dfce7cd155bd8be88b7e4bb10c0b5
David Gurovich <david.gurovich@outlook.com>
2023-02-02 14:52:25 -0800

    added security to links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,15 +12,15 @@ function Projects() {
         <div className='flex flex-col justify-center items-center mx-auto gap-16 '>
           <h1 className='text-center text-5xl my-10 text-white font-bold border-b-4 border-red-600'>My Work</h1>
           <div className='flex md:flex-row flex-col justify-center gap-5 px-20' >
-              <a href="https://davidgurovich.github.io/weatherapi/" target="_blank" className='md:h-auto md:w-auto w-screen px-4'>
+              <a rel="noopener noreferrer" href="https://davidgurovich.github.io/weatherapi/" target="_blank" className='md:h-auto md:w-auto w-screen px-4'>
                 <img src={weatherPhoto} alt="" className="hover:border-red-600 border-2 hover:cursor-pointer rounded-3xl shadow-lg shadow-blue-500/60"/>
                 </a>
               <div className=' pt-10 w-1/2 ml-10'>
                 <h1 className='text-white text-3xl font-bold mb-6'> Weather App</h1>
                 <p className='text-2xl text-white'> A weather app created using ReactJS, TailWindCSS, and the OpenWeatherMap API.  </p> 
                 <div className='flex flex-col gap-5 pt-10 font-bold text-xl '>
-                  <a rel="noopener" href="https://davidgurovich.github.io/weatherapi/" target="_blank" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-neutral-900 text-white'>Live Project</a>
-                  <a rel="noopener" href="https://github.com/DavidGurovich/weatherapi" target="_blank" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-neutral-900 text-white'>View Code</a>
+                  <a rel="noopener noreferrer" href="https://davidgurovich.github.io/weatherapi/" target="_blank" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-neutral-900 text-white'>Live Project</a>
+                  <a rel="noopener noreferrer" href="https://github.com/DavidGurovich/weatherapi" target="_blank" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-neutral-900 text-white'>View Code</a>
                 </div>
               </div>
           </div>
@@ -30,11 +30,11 @@ function Projects() {
                 <p className='text-2xl  text-white relative'>One of the first projects I created while learning
                 programming through the Odin Project. Made using only JavaScript, CSS, and HTML. I avoided using the </p>
                 <div className='flex flex-col gap-5 pt-10 font-bold text-xl'>
-                  <a href="https://davidgurovich.github.io/Calculator/" target="_blank" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-neutral-900 text-white'>Live Project</a>
-                  <a href="https://github.com/DavidGurovich/Calculator" target="_blank" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-neutral-900 text-white'>View Code</a>
+                  <a rel="noopener noreferrer" href="https://davidgurovich.github.io/Calculator/" target="_blank" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-neutral-900 text-white'>Live Project</a>
+                  <a rel="noopener noreferrer" href="https://github.com/DavidGurovich/Calculator" target="_blank" className='w-fit px-5 py-2 border-b-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-neutral-900 text-white'>View Code</a>
                 </div>
               </div>
-              <a href="https://davidgurovich.github.io/Calculator/" target="_blank" className='md:h-auto md:w-auto w-screen px-4' >
+              <a rel="noopener noreferrer" href="https://davidgurovich.github.io/Calculator/" target="_blank" className='md:h-auto md:w-auto w-screen px-4' >
               <img src={calcPhoto} alt="Calculator Image" className="hover:border-red-600 border-2 hover:cursor-pointer duration-500 rounded-3xl shadow-lg shadow-red-500/60"/>
               </a>
           </div>
